c2737cd074377854f5cc9fd2f96cfaf46748ecd8
Brian Woods <me@brian.biz>
2014-01-16 21:29:26 -0800

    foo

--- src/ios/CDVIDChecker.m
+++ src/ios/CDVIDChecker.m
@@ -78,98 +78,98 @@
 
 - (void)captureCredentials:(CDVInvokedUrlCommand *)command
 {
-//  NSString *country = [command.arguments objectAtIndex:0];
-//  NSString *type = [command.arguments objectAtIndex:1];
-//  IDCDocType docType;
-//  
-//  if ([type isEqualToString:@"DriversLicense"]) {
-//      docType = kIDCDocTypeDriversLicense;
-//  } else if ([type isEqualToString:@"2DBarCode"] ) {
-//      docType = kIDCDocType2DBarcode;
-//  } else {
-//      docType = kIDCDocTypePassport;
-//  }
-//  
-//  //doc type and country are not used yet but will be in the future
-//  IDCDocument *doc = [[IDCDocument alloc] initWithDocType:docType country:country];
-//  doc.cameraHelpText = _cameraHelpText;
-//  doc.documentDimensions = CGSizeMake(85.6, 54.f);
-//  
-//  
-//  [[IDCheckerSDK shared] startProcessForDocument:doc viewControllerToPresent:self.viewController quality:kIDCQualityTypeMedium pictureTakenBlock:^(BOOL pictureTaken) {
-//      if(pictureTaken){
-//          //Callback That Picture is Taken
-//          [self showWait:YES];
-//      }
-//  } uploadDoneBlock:^(IDCDocument *doc) {
-//      if(!doc.error) {
-//          // Callback with data
-//          //we save the guid so we can use it to refresh the data later
-//          _currentDoc = doc;
-//          [[IDCheckerSDK shared] uploadColorImageForIDCProcessing:doc.originalImage guid:doc.guid block:^(NSError *error) {
-//              if(error)
-//              {
-//                  [self showWait:NO];
-//                  CDVPluginResult *res = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-//                  [self.commandDelegate sendPluginResult:res callbackId:command.callbackId];
-//              } else {
-//                  [[IDCheckerSDK shared] closeRecordForGuid:doc.guid block:^(NSError *error) {
-//                      if(error) {
-//                          [self showWait:NO];
-//                          
-//                          // Call error callback
-//                          CDVPluginResult *res = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-//                          [self.commandDelegate sendPluginResult:res callbackId:command.callbackId];
-//                      } else {
-//                          // Start polling for more data
-//                          [[IDCheckerSDK shared] startPollingForMoreAccurateData:_currentDoc pollingResultBlock:^(IDCDocument *result) {
-//                              //we got a result;
-//                              [self showWait:NO];
+  NSString *country = [command.arguments objectAtIndex:0];
+  NSString *type = [command.arguments objectAtIndex:1];
+  IDCDocType docType;
+  
+  if ([type isEqualToString:@"DriversLicense"]) {
+      docType = kIDCDocTypeDriversLicense;
+  } else if ([type isEqualToString:@"2DBarCode"] ) {
+      docType = kIDCDocType2DBarcode;
+  } else {
+      docType = kIDCDocTypePassport;
+  }
+  
+  //doc type and country are not used yet but will be in the future
+  IDCDocument *doc = [[IDCDocument alloc] initWithDocType:docType country:country];
+  doc.cameraHelpText = _cameraHelpText;
+  doc.documentDimensions = CGSizeMake(85.6, 54.f);
+  
+  
+  [[IDCheckerSDK shared] startProcessForDocument:doc viewControllerToPresent:self.viewController quality:kIDCQualityTypeMedium pictureTakenBlock:^(BOOL pictureTaken) {
+      if(pictureTaken){
+          //Callback That Picture is Taken
+          [self showWait:YES];
+      }
+  } uploadDoneBlock:^(IDCDocument *doc) {
+      if(!doc.error) {
+          // Callback with data
+          //we save the guid so we can use it to refresh the data later
+          _currentDoc = doc;
+          [[IDCheckerSDK shared] uploadColorImageForIDCProcessing:doc.originalImage guid:doc.guid block:^(NSError *error) {
+              if(error)
+              {
+                  [self showWait:NO];
+                  CDVPluginResult *res = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+                  [self.commandDelegate sendPluginResult:res callbackId:command.callbackId];
+              } else {
+                  [[IDCheckerSDK shared] closeRecordForGuid:doc.guid block:^(NSError *error) {
+                      if(error) {
+                          [self showWait:NO];
+                          
+                          // Call error callback
+                          CDVPluginResult *res = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+                          [self.commandDelegate sendPluginResult:res callbackId:command.callbackId];
+                      } else {
+                          // Start polling for more data
+                          [[IDCheckerSDK shared] startPollingForMoreAccurateData:_currentDoc pollingResultBlock:^(IDCDocument *result) {
+                              //we got a result;
+                              [self showWait:NO];
 
-//                              
-//                              NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-//                              [dateFormatter setDateStyle:NSDateFormatterMediumStyle];
-//                              
-//                              // turn the images into something we can handle/post back to the server to save
-//                              NSData *origImageData = UIImageJPEGRepresentation(result.originalImage, 1.0);
-//                              NSData *passPhotoData = UIImageJPEGRepresentation(result.passPhoto, 1.0);
-//                              NSData *processedImageData = UIImageJPEGRepresentation(result.processedImage, 1.0);
-//                              NSString *origImage64 = [origImageData base64Encoding];
-//                              NSString *passPhoto64 = [passPhotoData base64Encoding];
-//                              NSString *processed64 = [processedImageData base64Encoding];
-//                              
-//                              NSMutableDictionary *dict = [[NSMutableDictionary alloc] initWithCapacity:17];
-//                              if (result.clientRef) [dict setObject:result.clientRef forKey:@"ClientRef"];
-//                              if (result.firstName) [dict setObject:result.firstName forKey:@"FirstName"];
-//                              if (result.lastName) [dict setObject:result.lastName forKey:@"LastName"];
-//                              if (result.address1) [dict setObject:result.address1 forKey:@"Address1"];
-//                              if (result.address2) [dict setObject:result.address2 forKey:@"Address2"];
-//                              if (result.doB) [dict setObject:[dateFormatter stringFromDate:result.doB] forKey:@"DOB"];
-//                              if (result.expDate) [dict setObject:[dateFormatter stringFromDate:result.expDate] forKey:@"Expiration"];
-//                              if (result.countryResultAbbrevation) [dict setObject:result.countryResultAbbrevation forKey:@"Country"];
-//                              if (result.socialSecurityNumber) [dict setObject:result.socialSecurityNumber forKey:@"SSN"];
-//                              if (result.documentNumber) [dict setObject:result.documentNumber forKey:@"DocumentNumber"];
-//                              if (result.issueDate) [dict setObject:[dateFormatter stringFromDate:result.issueDate] forKey:@"IssueDate"];
-//                              if (passPhoto64) [dict setObject:passPhoto64 forKey:@"PassPhoto"];
-//                              if (origImage64) [dict setObject:origImage64 forKey:@"OrigPhoto"];
-//                              if (processed64) [dict setObject:processed64 forKey:@"ProcessedPhoto"];
-//                              if (result.nationality) [dict setObject:result.nationality forKey:@"Nationality"];
-//                              if (result.guid) [dict setObject:result.guid forKey:@"GUID"];
-//                              
-//                              // build result into data structure to return via callback
-//                              CDVPluginResult *res = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:dict];
-//                              [self.commandDelegate sendPluginResult:res callbackId:command.callbackId];
-//                          }];
-//                      }
-//                  }];
-//              }
-//          }];
-//      } else {
-//          // Call error Callback
-//          CDVPluginResult *res = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-//          [self.commandDelegate sendPluginResult:res callbackId:command.callbackId];
-//      }
-//  }];
+                              
+                              NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+                              [dateFormatter setDateStyle:NSDateFormatterMediumStyle];
+                              
+                              // turn the images into something we can handle/post back to the server to save
+                              NSData *origImageData = UIImageJPEGRepresentation(result.originalImage, 1.0);
+                              NSData *passPhotoData = UIImageJPEGRepresentation(result.passPhoto, 1.0);
+                              NSData *processedImageData = UIImageJPEGRepresentation(result.processedImage, 1.0);
+                              NSString *origImage64 = [origImageData base64Encoding];
+                              NSString *passPhoto64 = [passPhotoData base64Encoding];
+                              NSString *processed64 = [processedImageData base64Encoding];
+                              
+                              NSMutableDictionary *dict = [[NSMutableDictionary alloc] initWithCapacity:17];
+                              if (result.clientRef) [dict setObject:result.clientRef forKey:@"ClientRef"];
+                              if (result.firstName) [dict setObject:result.firstName forKey:@"FirstName"];
+                              if (result.lastName) [dict setObject:result.lastName forKey:@"LastName"];
+                              if (result.address1) [dict setObject:result.address1 forKey:@"Address1"];
+                              if (result.address2) [dict setObject:result.address2 forKey:@"Address2"];
+                              if (result.doB) [dict setObject:[dateFormatter stringFromDate:result.doB] forKey:@"DOB"];
+                              if (result.expDate) [dict setObject:[dateFormatter stringFromDate:result.expDate] forKey:@"Expiration"];
+                              if (result.countryResultAbbrevation) [dict setObject:result.countryResultAbbrevation forKey:@"Country"];
+                              if (result.socialSecurityNumber) [dict setObject:result.socialSecurityNumber forKey:@"SSN"];
+                              if (result.documentNumber) [dict setObject:result.documentNumber forKey:@"DocumentNumber"];
+                              if (result.issueDate) [dict setObject:[dateFormatter stringFromDate:result.issueDate] forKey:@"IssueDate"];
+                              if (passPhoto64) [dict setObject:passPhoto64 forKey:@"PassPhoto"];
+                              if (origImage64) [dict setObject:origImage64 forKey:@"OrigPhoto"];
+                              if (processed64) [dict setObject:processed64 forKey:@"ProcessedPhoto"];
+                              if (result.nationality) [dict setObject:result.nationality forKey:@"Nationality"];
+                              if (result.guid) [dict setObject:result.guid forKey:@"GUID"];
+                              
+                              // build result into data structure to return via callback
+                              CDVPluginResult *res = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:dict];
+                              [self.commandDelegate sendPluginResult:res callbackId:command.callbackId];
+                          }];
+                      }
+                  }];
+              }
+          }];
+      } else {
+          // Call error Callback
+          CDVPluginResult *res = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+          [self.commandDelegate sendPluginResult:res callbackId:command.callbackId];
+      }
+  }];
 }
 
 
