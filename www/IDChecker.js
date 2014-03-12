1fc4cac04a654984ccbe059f0f0dc7ef7fb03885
Brian Woods <me@brian.biz>
2014-03-12 14:24:37 -0700

    fixing to comply to cordova plugin spec

--- www/IDChecker.js
+++ www/IDChecker.js
@@ -1,42 +1,34 @@
-/*
- *
-*/
+var argscheck = require('cordova/argscheck'),
+    channel = require('cordova/channel'),
+    utils = require('cordova/utils'),
+    exec = require('cordova/exec'),
+    cordova = require('cordova');
 
-cordova.define("com.idchecker.cordova.IDChecker", function(require, exports, module) {
-               
-  var argscheck = require('cordova/argscheck'),
-      channel = require('cordova/channel'),
-      utils = require('cordova/utils'),
-      exec = require('cordova/exec'),
-      cordova = require('cordova');
+/**
+  * pass in required arguments for the client
+ */
+function IDChecker(agent, apiToken, clientRef, password, userId, cameraHelpText) {
+  var me = this;
+  var callback = function() {};
+  exec(callback, callback, 'CDVIDChecker', 'initializeClientCredentials', [ agent, apiToken, clientRef, password, userId, cameraHelpText ]);
+};
 
-  /**
-    * pass in required arguments for the client
-   */
-  function IDChecker(agent, apiToken, clientRef, password, userId, cameraHelpText) {
-    var me = this;
-    var callback = function() {};
-    exec(callback, callback, 'CDVIDChecker', 'initializeClientCredentials', [ agent, apiToken, clientRef, password, userId, cameraHelpText ]);
-  };
+/**
+  * Get the scanned document. 
+  * type must be one of "Passport", "DriversLicense", or "2DBarCode".  Country is a 2 letter country code
+ */
+IDChecker.prototype.captureFront = function(success, failure, country, type) {
+  exec(success, failure, 'CDVIDChecker', 'captureFront', [country, type]);
+};
 
-  /**
-    * Get the scanned document. 
-    * type must be one of "Passport", "DriversLicense", or "2DBarCode".  Country is a 2 letter country code
-   */
-  IDChecker.prototype.captureFront = function(success, failure, country, type) {
-    exec(success, failure, 'CDVIDChecker', 'captureFront', [country, type]);
-  };
+IDChecker.prototype.captureFrontAndBack = function(success, failure, country, type) {
+  exec(success, failure, 'CDVIDChecker', 'captureFrontAndBack', [country, type]);
+};
 
-  IDChecker.prototype.captureFrontAndBack = function(success, failure, country, type) {
-    exec(success, failure, 'CDVIDChecker', 'captureFrontAndBack', [country, type]);
-  };
+             
+IDChecker.prototype.presentCameraForButton = function(success, failure, country, type) {
+  //exec(success, failure, 'CDVIDChecker', 'presentCameraForButton', [country, type]);
+  exec(success, failure, 'CDVIDChecker', 'presentCameraForButton', [country, "2DBarCode"]);
+};
 
-               
-  IDChecker.prototype.presentCameraForButton = function(success, failure, country, type) {
-    //exec(success, failure, 'CDVIDChecker', 'presentCameraForButton', [country, type]);
-    exec(success, failure, 'CDVIDChecker', 'presentCameraForButton', [country, "2DBarCode"]);
-  };
-  module.exports = IDChecker;
-               
-});
-
+module.exports = IDChecker;
